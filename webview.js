47bb413caf66f1b9d7d5daf3139a09524db87682
Alex Devero <deveroalex@gmail.com>
2020-10-31 08:52:46 +0100

    Load service css

--- webview.js
+++ webview.js
@@ -1,1 +1,9 @@
 'use strict'
+
+var _path = _interopRequireDefault(require("path"))
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = Franz => {
+  Franz.injectCSS(_path.default.join(__dirname, 'service.css'))
+}
